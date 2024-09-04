76aefb0b92107d66d4f4be58893bf3b33d113ec3
hjoo830 <hjoo830@naver.com>
2024-09-04 09:36:07 +0900

    ✨ [9/4] 배열 주요 메소드

---
+++ 24-09-04/array.js
@@ -0,0 +1,12 @@
+// 배열 생성
+const fruits = ["apple", "banana", "orange"];
+
+// 요소 추가
+fruits.push("grape");
+
+// 요소 제거
+const lastFruit = fruits.pop();
+
+// 배열 출력
+console.log(fruits);
+console.log(lastFruit);
