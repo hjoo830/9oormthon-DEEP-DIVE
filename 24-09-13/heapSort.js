fd8503b6954de034491c4228fd9d49b537822a73
hjoo830 <hjoo830@naver.com>
2024-09-14 13:14:54 +0900

    ✨ [9/13] heapSort

---
+++ 24-09-13/heapSort.js
@@ -0,0 +1,31 @@
+function heapSort(arr) {
+  let n = arr.length;
+
+  for (let i = Math.floor(n / 2) - 1; i >= 0; i--) {
+    heapify(arr, n, i);
+  }
+
+  for (let i = n - 1; i > 0; i--) {
+    [arr[0], arr[i]] = [arr[i], arr[0]];
+    heapify(arr, i, 0);
+  }
+
+  return arr;
+}
+
+function heapify(arr, n, i) {
+  let largest = i;
+  const left = 2 * i + 1;
+  const right = 2 * i + 2;
+
+  if (left < n && arr[left] > arr[largest]) largest = left;
+  if (right < n && arr[right] > arr[largest]) largest = right;
+
+  if (largest !== i) {
+    [arr[i], arr[largest]] = [arr[largest], arr[i]];
+    heapify(arr, n, largest);
+  }
+}
+
+const nums = [5, 3, 8, 4, 2];
+console.log("힙 정렬 결과:", heapSort(nums));
