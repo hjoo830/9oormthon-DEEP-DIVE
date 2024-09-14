dbddf8eff5463f8eb1ebde9be026f5130c340aa6
hjoo830 <hjoo830@naver.com>
2024-09-14 13:14:44 +0900

    ✨ [9/13] mergeSort

---
+++ 24-09-13/mergeSort.js
@@ -0,0 +1,28 @@
+function mergeSort(arr) {
+  if (arr.length <= 1) return arr;
+
+  const middle = Math.floor(arr.length / 2);
+  const left = mergeSort(arr.slice(0, middle));
+  const right = mergeSort(arr.slice(middle));
+
+  return merge(left, right);
+}
+
+function merge(left, right) {
+  const result = [];
+  let leftIndex = 0,
+    rightIndex = 0;
+
+  while (leftIndex < left.length && rightIndex < right.length) {
+    if (left[leftIndex] < right[rightIndex]) {
+      result.push(left[leftIndex++]);
+    } else {
+      result.push(right[rightIndex++]);
+    }
+  }
+
+  return result.concat(left.slice(leftIndex)).concat(right.slice(rightIndex));
+}
+
+const nums = [5, 3, 8, 4, 2];
+console.log("병합 정렬 결과:", mergeSort(nums));
