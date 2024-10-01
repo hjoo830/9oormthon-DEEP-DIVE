959dc29849bd1316206642b442022837e43b7cf9
hjoo830 <hjoo830@naver.com>
2024-10-01 10:23:29 +0900

    ✨ [10/1] String Methods

---
+++ 24-10-01/stringMethods.js
@@ -0,0 +1,16 @@
+// toUpperCase()
+const greeting = "hello";
+console.log("toUpperCase():", greeting.toUpperCase()); // "HELLO"
+
+// split()
+const sentence = "Hello world";
+const words = sentence.split(" ");
+console.log("split():", words); // ["Hello", "world"]
+
+// includes()
+const sentence2 = "JavaScript is fun";
+console.log("includes():", sentence2.includes("fun")); // true
+
+// replace()
+const newSentence = sentence2.replace("JavaScript", "coding");
+console.log("replace():", newSentence); // "coding is fun"
