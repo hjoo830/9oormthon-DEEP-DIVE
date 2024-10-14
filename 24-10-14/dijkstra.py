07ada6927fd12ee10fdfacc59773f88a2e00173b
hjoo830 <hjoo830@naver.com>
2024-10-14 23:24:57 +0900

    ✨ [10/14] Dijkstra

---
+++ 24-10-14/dijkstra.py
@@ -0,0 +1,33 @@
+import heapq
+
+def dijkstra(graph, start):
+    distances = {node: float('inf') for node in graph}
+    distances[start] = 0
+    priority_queue = [(0, start)]
+    
+    while priority_queue:
+        current_distance, current_node = heapq.heappop(priority_queue)
+        
+        if current_distance > distances[current_node]:
+            continue
+        
+        for neighbor, weight in graph[current_node].items():
+            distance = current_distance + weight
+            
+            if distance < distances[neighbor]:
+                distances[neighbor] = distance
+                heapq.heappush(priority_queue, (distance, neighbor))
+    
+    return distances
+
+graph = {
+    'A': {'B': 1, 'C': 4},
+    'B': {'A': 1, 'C': 2, 'D': 7},
+    'C': {'A': 4, 'B': 2, 'D': 3},
+    'D': {'B': 7, 'C': 3}
+}
+start_node = 'A'
+result = dijkstra(graph, start_node)
+print('======dijkstra======')
+for node in result:
+    print(f"{start_node}에서 {node}까지 최단 거리: {result[node]}")
