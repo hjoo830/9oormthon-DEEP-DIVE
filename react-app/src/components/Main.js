4c5ef9d0e70b24009b9a05fd1091bc7045c3536c
hjoo830 <hjoo830@naver.com>
2024-10-18 09:10:05 +0900

    ✨ [10/18] 화면에 이름 출력하기

--- react-app/src/components/Main.js
+++ react-app/src/components/Main.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 function Main() {
-  return <div style={{ backgroundColor: 'gray', height: '500px' }}>Main</div>;
+  return <div style={{ backgroundColor: 'gray', height: '500px' }}>황효주</div>;
 }
 
 export default Main;
