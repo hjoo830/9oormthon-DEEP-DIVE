e3b500b609a6d7119c1a459964bd709542eb5fe5
hjoo830 <hjoo830@naver.com>
2024-10-22 09:23:15 +0900

    ✨ [10/22] state 생성 및 setState() 함수 사용하기

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,16 +1,31 @@
+import React, { useState } from "react";
 import { displayTodos } from "./components/displayTodos";
 
-const todos = [
-  { id: 1, task: "구름톤 출석하기", completed: true },
-  { id: 2, task: "밥 먹기", completed: false },
-  { id: 3, task: "모던 리액트 완독하기", completed: false },
-];
-
 function App() {
+  const [todos, setTodos] = useState([
+    { id: 1, task: "구름톤 출석하기", completed: true },
+    { id: 2, task: "밥 먹기", completed: false },
+    { id: 3, task: "모던 리액트 완독하기", completed: false },
+  ]);
+
+  const toggleTodo = (id) => {
+    const updatedTodos = todos.map((todo) => {
+      if (todo.id === id) {
+        return {
+          id: todo.id,
+          task: todo.task,
+          completed: !todo.completed,
+        };
+      }
+      return todo;
+    });
+    setTodos(updatedTodos);
+  };
+
   return (
     <div className="App">
       <h1 style={{ textAlign: "center" }}>To Do List</h1>
-      <ul>{displayTodos(todos)}</ul>
+      <ul>{displayTodos(todos, toggleTodo)}</ul>
     </div>
   );
 }
--- react-app/src/components/displayTodos.js
+++ react-app/src/components/displayTodos.js
@@ -1,8 +1,8 @@
 import React from "react";
 
-export function displayTodos(todoList) {
+export function displayTodos(todoList, toggleTodo) {
   return todoList.map((todo) => (
-    <li key={todo.id}>
+    <li key={todo.id} onClick={() => toggleTodo(todo.id)}>
       {todo.completed ? "✅" : "❌"} {todo.task}
     </li>
   ));
