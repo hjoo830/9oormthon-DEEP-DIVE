f91de62956293dedbecb88793ed00152dbc88b7d
hjoo830 <hjoo830@naver.com>
2024-10-25 09:18:10 +0900

    🚚 [10/25] 컴포넌트 이름을 DisplayTodos로 변경

--- react-app/src/components/displayTodos.js
+++ react-app/src/components/displayTodos.js
@@ -1,6 +1,8 @@
 import React from "react";
 
-export function displayTodos(todoList, toggleTodo) {
+// 컴포넌트 이름을 네이밍 컨벤션에 따라 DisplayTodos로 변경
+// git conflict를 위해 같은 파일에 다른 내용(주석) 추가
+export function DisplayTodos(todoList, toggleTodo) {
   return todoList.map((todo) => (
     <li key={todo.id} onClick={() => toggleTodo(todo.id)}>
       {todo.completed ? "✅" : "❌"} {todo.task}
