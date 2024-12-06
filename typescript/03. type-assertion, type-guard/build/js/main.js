dd1a6deb024b129c008ce7c31fd9163815f32511
hjoo830 <hjoo830@naver.com>
2024-12-06 11:08:15 +0900

    ✨ 03. type-assertion, type-guard

---
+++ typescript/03. type-assertion, type-guard/build/js/main.js
@@ -0,0 +1,28 @@
+"use strict";
+let foo = {};
+foo.bar = 123;
+foo.bas = "hello";
+// const bodyElement = document.querySelector("body");
+// bodyElement?.innerText = "Hello"; // null 일 수 있다고 error
+// 타입 단언
+const bodyElement = document.querySelector("body");
+bodyElement.innerText = "Hello";
+// ! not null 타입 단언
+const bodyElement1 = document.querySelector("body");
+bodyElement1.innerText = "Hello";
+// type guard
+const bodyElement2 = document.querySelector("body");
+if (bodyElement2) {
+    bodyElement2.innerText = "Hello";
+}
+// 잘못된 예시
+function func(arg) {
+    // return arg.toLowerCase(); // arg는 null일 수 있어서 error
+    // return (arg as string).toLowerCase(); // 이렇게 하면 여기에선 에러 안떠도 실행하면 Uncaught TypeError: arg.toLowerCase is not a function
+    // type guard 사용 (올바른 예시)
+    if (arg) {
+        return arg.toLowerCase();
+    }
+}
+func("hello");
+func(null);
